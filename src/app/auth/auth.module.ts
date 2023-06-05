11676714b61ff64e7b33f627d7b9ab98a114e7f4
Esteban Secaida <esecaidavargas@gmail.com>
2023-06-05 10:03:23 -0600

    1.2 - login shell done

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignPageComponent } from './pages/sign-page/sign-page.component';
+
 
 
 
@@ -17,7 +20,8 @@ import { SignPageComponent } from './pages/sign-page/sign-page.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
