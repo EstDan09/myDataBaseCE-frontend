3a6e12a1c22ded30307e57a3f4aceb77a5150df0
Esteban Secaida <esecaidavargas@gmail.com>
2023-06-05 09:04:38 -0600

    1.1

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignPageComponent } from './pages/sign-page/sign-page.component';
 
 const routes: Routes = [
   {
     path: '', //localhost:4200/auth/
-    component: LayoutPageComponent
+    component: LayoutPageComponent,
+    children: [
+      {path: 'login', component: LoginPageComponent},
+      {path: 'sign', component: SignPageComponent},
+      {path: '**', redirectTo: 'login'}
+    ]
   }
 ];
 
