eeb472955c50c27e2427ac66f37ebe88d3e51c13
Esteban Secaida <esecaidavargas@gmail.com>
2023-06-10 23:52:20 -0600

    2.0

--- src/app/xmls/pages/main-page/main-page.component.ts
+++ src/app/xmls/pages/main-page/main-page.component.ts
@@ -15,8 +15,9 @@ export class MainPageComponent {
   words:string[] = ["hola"]
   dataGiven: string = ''
   arn:string[][] = [["hen", "hon", "wer"], ["hol", "art", "erw"], ["dad", "ddd", "wep"]]
-  colData : Columnoide[] = []
-  colDataTemp : Columnoide[] = []
+  colData : Columnoide[] = [{'Columna1': "Title", 'Columna2': "Values"}]
+  colDataTemp2 : Columnoide[] = []
+  colDataTemp: Columnoide[] = []
   displayedColumns: string[] = ['Columna1', 'Columna2', 'Columna3', 'Columna4','Columna5','Columna6'];
 
 
@@ -33,10 +34,10 @@ export class MainPageComponent {
     })
 
     console.log(this.words[0]);
-
-    this.colData = [{'Columna1': this.arn[0][0], 'Columna2':this.arn[0][1], 'Columna3':this.arn[0][2]},
-    {'Columna1': this.arn[1][0], 'Columna2':this.arn[1][1], 'Columna3':this.arn[1][2]},
-    {'Columna1': this.arn[2][0], 'Columna2':this.arn[2][1], 'Columna3':this.arn[2][2]}]
+    
+    //this.colData = [{'Columna1': this.arn[0][0], 'Columna2':this.arn[0][1], 'Columna3':this.arn[0][2]},
+    //{'Columna1': this.arn[1][0], 'Columna2':this.arn[1][1], 'Columna3':this.arn[1][2]},
+    //{'Columna1': this.arn[2][0], 'Columna2':this.arn[2][1], 'Columna3':this.arn[2][2]}]
 
   }
 
@@ -48,13 +49,42 @@ export class MainPageComponent {
   }
 
   diosPorfa(){
-    this.miBackendService.getUser()
+    this.miBackendService.getXml().subscribe( resultado =>{
+      this.arn = resultado;
+    })
     console.log(this.arn[0].length)
 
-    this.colData = [{'Columna1': this.arn[0][0], 'Columna2':this.arn[0][1], 'Columna3':this.arn[0][2]},
-    {'Columna1': "fer", 'Columna2':this.arn[1][1], 'Columna3':this.arn[1][2]},
-    {'Columna1': this.arn[2][0], 'Columna2':this.arn[2][1], 'Columna3':this.arn[2][2]}]
+    let sizeOut= this.arn.length
 
+    if (sizeOut == 1){
+      for(let i = 0; i < sizeOut; i++){
+        this.colDataTemp = [{'Columna1' : this.arn[i][0] }]
+        this.colData = this.colData.concat(this.colDataTemp)
+      }
+    }
+
+    if (sizeOut == 2){
+      for(let i = 0; i < sizeOut; i++){
+        this.colDataTemp = [{'Columna1' : this.arn[i][0], 'Columna2': this.arn[i][1] }]
+        this.colData = this.colData.concat(this.colDataTemp)
+      }
+    }
+
+    if (sizeOut == 3){
+      for(let i = 0; i < sizeOut; i++){
+        this.colDataTemp = [{'Columna1' : this.arn[i][0], 'Columna2': this.arn[i][1], 'Columna3': this.arn[i][2] }]
+        this.colData = this.colData.concat(this.colDataTemp)
+      }
+    }
+
+    if (sizeOut == 4){
+      for(let i = 0; i < sizeOut; i++){
+        this.colDataTemp = [{'Columna1' : this.arn[i][0], 'Columna2': this.arn[i][1], 'Columna3': this.arn[i][2],
+      'Columna4': this.arn[i][3] }]
+        this.colData = this.colData.concat(this.colDataTemp)
+      }
+    }
+    
     console.log(this.colData[0].Columna1)
     
       
